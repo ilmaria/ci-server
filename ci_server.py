62b415695e8786f50748e0326f46e0d8f640ea92
Ilmari Autio <ilmari.autio@gmail.com>
2017-06-18 11:28:08 +0000

    Hmac now works

--- ci_server.py
+++ ci_server.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
 import os
 import json
 import hmac
 import hashlib
 
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -12,17 +12,18 @@ def github_event():
     if request.headers.get('X-Github-Event') != 'push':
         return ''
 
+    raw_data = request.get_data()
+    hex_string = hmac.new(os.environ.get('GITHUB_KEY'),
+                          raw_data, hashlib.sha1).hexdigest()
+
+    if hmac.compare_digest(request.headers.get('X-Hub-Signature'), 'sha1=' + hex_string):
+        return ''
+
     payload = json.loads(request.form.get('payload'))
 
     if payload.get('ref') != 'refs/heads/master':
         return ''
 
-    hexa = hmac.new(os.environ.get('GITHUB_KEY'), json.dumps(
-        request.form), hashlib.sha1).hexdigest()
-
-    print(hexa)
-    print(request.headers.get('X-Hub-Signature'))
-
-    print(request.data)
+    print('all good')
 
     return ''
