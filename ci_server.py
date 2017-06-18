637571eba9603d720f3db6f5c2a34d707747f63b
Ilmari Autio <ilmari.autio@gmail.com>
2017-06-18 10:54:57 +0000

    Check for correct github event

--- ci_server.py
+++ ci_server.py
@@ -1,8 +1,25 @@
 from flask import Flask, request
+import json
+import hmac
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
-def hello_world():
-    print(request.form)
-    return 'Hello, World!'
+def github_event():
+    if request.headers.get('X-Github-Event') != 'push':
+        return ''
+
+    payload = json.loads(request.form.get('payload'))
+
+    if payload.get('ref') != 'refs/heads/master':
+        return ''
+
+    hexa = hmac.new(b'testisalaisuus', digestmod='sha1').hexdigest()
+
+    print(hexa)
+    print(request.headers.get('X-Github-Signature'))
+
+    print(json.dumps(payload, indent=2))
+
+    return ''
