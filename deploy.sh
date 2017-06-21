4d0b8df4c8d7baf3e8f516c50bd0994bbd6fc085
Ilmari Autio <ilmari.autio@gmail.com>
2017-06-21 20:05:21 +0000

    Change file permissions with deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,10 @@ cd ../cloud_drive
 echo "y\ny\ny\n" | mix deps.get
 echo "y\ny\ny\n" | MIX_ENV=prod mix release --env=prod --no-tar
 
+# Allow 'cloud-drive' user to modify this file. The 'cloud_drive' executable
+# will want to modify this.
+chmod -R o+w _build/prod/rel/cloud_drive/releases/start_erl.data
+
 # Start the server daemon as 'cloud-drive' user. Use `bin/cloud_drive remote_console`
 # to connect to the server's shell.
 sudo -u cloud-drive _build/prod/rel/cloud_drive/bin/cloud_drive start
